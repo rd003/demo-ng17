739e823ac0179c522b0511404454b81304bde77b
Ravindra Devrani <ravindra.devrani003@gmail.com>
2023-12-01 13:30:34 +0530

    feat: person service

---
+++ src/app/shared/data-access/person.service.ts
@@ -0,0 +1,32 @@
+import { Injectable, inject } from "@angular/core";
+import { PersonModel } from "../models/person.model";
+import { HttpClient } from "@angular/common/http";
+
+@Injectable({ providedIn: "root" })
+export class PersonService {
+  private readonly url = "http://localhost:3000/people";
+  private readonly http = inject(HttpClient);
+
+  add(person: PersonModel) {
+    return this.http.post<PersonModel>(this.url, person);
+  }
+
+  update(person: PersonModel) {
+    const url = `${this.url}/${person.id}`;
+    return this.http.put<any>(url, person);
+  }
+
+  get(id: string) {
+    const url = `${this.url}/${id}`;
+    return this.http.get<PersonModel>(url);
+  }
+
+  delete(id: string) {
+    const url = `${this.url}/${id}`;
+    return this.http.delete<PersonModel>(url);
+  }
+
+  getAll() {
+    return this.http.get<PersonModel[]>(this.url);
+  }
+}
---
+++ src/app/shared/models/person.model.ts
@@ -0,0 +1,5 @@
+export interface PersonModel {
+  id: string;
+  name: string;
+  email: string;
+}
