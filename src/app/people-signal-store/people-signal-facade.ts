5aaaf230c049e0a690dc281eaea46436976ae81a
Ravindra Devrani <ravindra.devrani003@gmail.com>
2023-12-07 15:28:23 +0530

    feat(people-signal-facade): start

--- src/app/people-signal-store/people-signal-facade.ts
+++ src/app/people-signal-store/people-signal-facade.ts
@@ -1,9 +1,55 @@
-import { signalState } from "@ngrx/signals";
+import { patchState, signalState } from "@ngrx/signals";
+import { PersonModel } from "../shared/models/person.model";
+import { inject } from "@angular/core";
+import { PersonService } from "../shared/data-access/person.service";
+import { HttpErrorResponse } from "@angular/common/http";
+import { takeUntilDestroyed } from "@angular/core/rxjs-interop";
 
+interface PersonState {
+  people: PersonModel[];
+  loading: boolean;
+  error: HttpErrorResponse | null;
+}
 export class PeopleSignalFacade {
-  private state = signalState({
+  private readonly personService = inject(PersonService);
+
+  private state = signalState<PersonState>({
     people: [],
     loading: false,
     error: null,
   });
+
+  people = this.state.people;
+  loading = this.state.loading;
+  error = this.state.error;
+
+  add(person: PersonModel) {
+    patchState(this.state, (state) => ({
+      people: {
+        ...state.people,
+        person,
+      },
+      loading: false,
+    }));
+  }
+
+  setLoading() {
+    patchState(this.state, { loading: true });
+  }
+
+  setError(error: HttpErrorResponse) {
+    patchState(this.state, { error });
+  }
+
+  constructor() {
+    const people$ = this.personService.getAll();
+    people$.pipe(takeUntilDestroyed()).subscribe({
+      next: (data) => {
+        patchState(this.state, { loading: false, people: data });
+      },
+      error: (error) => {
+        this.setError(error);
+      },
+    });
+  }
 }
