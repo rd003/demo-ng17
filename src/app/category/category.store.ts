4ce748d22f7cb3e65e2d88a99f82d12e0ec5b52c
Ravindra Devrani <ravindra.devrani003@gmail.com>
2023-12-20 20:14:08 +0530

    feat: load people category store

---
+++ src/app/category/category.store.ts
@@ -0,0 +1,60 @@
+import { HttpErrorResponse } from "@angular/common/http";
+import { Category } from "../shared/models/category";
+import {
+  patchState,
+  signalStore,
+  withHooks,
+  withMethods,
+  withState,
+} from "@ngrx/signals";
+import { inject } from "@angular/core";
+import { CategoryService } from "../shared/data-access/category.service";
+import { rxMethod } from "@ngrx/signals/rxjs-interop";
+import { pipe, switchMap, tap } from "rxjs";
+import { tapResponse } from "@ngrx/operators";
+
+type State = {
+  categories: Category[];
+  loading: boolean;
+  error: HttpErrorResponse | null;
+};
+
+const initialState: State = {
+  categories: [],
+  loading: false,
+  error: null,
+};
+
+export const CategoryStore = signalStore(
+  withState(initialState),
+  withMethods((store, categoryService = inject(CategoryService)) => ({
+    loadPeople: rxMethod<void>(
+      pipe(
+        tap(() => {
+          patchState(store, { loading: true });
+        }),
+        switchMap(() => {
+          return categoryService.getAll().pipe(
+            tapResponse({
+              next: (categories) => {
+                patchState(store, { categories });
+              },
+              error: (error: HttpErrorResponse) => {
+                console.log(error);
+                patchState(store, { error });
+              },
+              finalize: () => {
+                patchState(store, { loading: false });
+              },
+            })
+          );
+        })
+      )
+    ),
+  })),
+  withHooks({
+    onInit({ loadPeople }) {
+      loadPeople();
+    },
+  })
+);
