ea977c8b99b5c66811e1f5b41093bf48c2afeb6c
Ravindra Devrani <ravindra.devrani003@gmail.com>
2023-12-22 20:05:09 +0530

    feat: category form ui

--- src/app/category/category.component.ts
+++ src/app/category/category.component.ts
@@ -2,14 +2,16 @@ import { NgIf } from "@angular/common";
 import { ChangeDetectionStrategy, Component, inject } from "@angular/core";
 import { CategoryStore } from "./category.store";
 import { CategoryListComponent } from "./ui/categoriy-list.component";
+import { CategoryFormComponent } from "./ui/category-form.component";
+import { Category } from "../shared/models/category";
 
 @Component({
   selector: `app-category`,
   standalone: true,
-  imports: [NgIf, CategoryListComponent],
+  imports: [NgIf, CategoryListComponent, CategoryFormComponent],
   template: `
     <h2>Category</h2>
-
+    <app-category-form (submit)="onSubmit($event)" />
     <app-category-list [categories]="categoryStore.categories()" />
   `,
   styles: [],
@@ -18,4 +20,7 @@ import { CategoryListComponent } from "./ui/categoriy-list.component";
 })
 export class CategoryComponent {
   categoryStore = inject(CategoryStore);
+  onSubmit(category: Category) {
+    alert(JSON.stringify(category));
+  }
 }
---
+++ src/app/category/ui/category-form.component.ts
@@ -0,0 +1,75 @@
+import {
+  ChangeDetectionStrategy,
+  Component,
+  Output,
+  inject,
+  EventEmitter,
+} from "@angular/core";
+import { Category } from "../../shared/models/category";
+import { FormBuilder, ReactiveFormsModule, Validators } from "@angular/forms";
+import { MatInputModule } from "@angular/material/input";
+import { MatButtonModule } from "@angular/material/button";
+import { NgIf } from "@angular/common";
+
+@Component({
+  standalone: true,
+  selector: "app-category-form",
+  imports: [ReactiveFormsModule, MatInputModule, MatButtonModule, NgIf],
+  template: `
+    <form [formGroup]="categoryForm" (ngSubmit)="onSumbit()">
+      <input type="hidden" formControlName="id" />
+      <div style="display:flex;gap:10px">
+        <mat-form-field>
+          <mat-label>Name</mat-label>
+          <input matInput formControlName="name" />
+        </mat-form-field>
+        <ng-container
+          *ngIf="f['name'].invalid && (f['name'].dirty || f['name'].touched)"
+        >
+          <div *ngIf="f['name'].errors?.['required']" class="text-danger">
+            Name is required
+          </div>
+        </ng-container>
+        <button
+          type="submit"
+          mat-raised-button
+          color="primary"
+          [disabled]="categoryForm.invalid"
+        >
+          Save
+        </button>
+        <button
+          type="button"
+          mat-raised-button
+          color="accent"
+          (click)="onReset()"
+        >
+          Reset
+        </button>
+      </div>
+    </form>
+  `,
+  styles: [``],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class CategoryFormComponent {
+  @Output() submit = new EventEmitter<Category>();
+  fb = inject(FormBuilder);
+  categoryForm = this.fb.group({
+    id: [""],
+    name: ["", Validators.required],
+  });
+
+  get f() {
+    return this.categoryForm.controls;
+  }
+
+  onSumbit() {
+    const category: Category = Object.assign(this.categoryForm.value);
+    this.submit.emit(category);
+  }
+
+  onReset() {
+    this.categoryForm.reset();
+  }
+}
