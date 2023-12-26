7a5fbaff2166cf0b6b29da568225054706a9f4ed
Ravindra Devrani <ravindra.devrani003@gmail.com>
2023-12-26 13:05:50 +0530

    building product form

--- src/app/product/product-form.component.ts
+++ src/app/product/product-form.component.ts
@@ -1,12 +1,22 @@
-import { ChangeDetectionStrategy, Component } from "@angular/core";
-import { CommonModule } from "@angular/common";
+import { ChangeDetectionStrategy, Component, inject } from "@angular/core";
+import { FormBuilder, Validators } from "@angular/forms";
+import { MatButtonModule } from "@angular/material/button";
+import { MatFormFieldModule } from "@angular/material/form-field";
+import { MatIconModule } from "@angular/material/icon";
 
 @Component({
   selector: "app-product-form",
   standalone: true,
-  imports: [CommonModule],
-  template: ` <p>product-form works!</p> `,
-  styles: ``,
+  imports: [MatFormFieldModule, MatIconModule, MatButtonModule],
+  template: ` <form></form> `,
+  styles: [``],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class ProductFormComponent {}
+export class ProductFormComponent {
+  fb = inject(FormBuilder);
+  productForm = this.fb.group({
+    id: [""],
+    name: ["", Validators.required],
+    category_id: ["", Validators.required],
+  });
+}
